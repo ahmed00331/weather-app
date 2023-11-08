b31ea3d106af9e149bb76b7200bb0a1a8a20f169
ahmed00331 <ahmedr00002@gmail.com>
2023-11-09 00:30:34 +0200

    final commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../src/app.dart';
+import '../src/core/utils/app_state_notifier.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    ChangeNotifierProvider<AppStateNotifier>(
+      create: (_) => AppStateNotifier(),
+      child: const MyApp(),
+    ),
+  );
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:weather/src/views/screens/bottom_nav_bar.dart';
+
+import 'core/constants/string_const.dart';
+import 'core/themes/theme_data.dart';
+import 'core/utils/app_state_notifier.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AppStateNotifier>(builder: (context, appState, child) {
+      return MaterialApp(
+        title: appName,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeConfig.themeSeed,
+        // darkTheme: ThemeConfig.darkTheme,
+        // themeMode: appState.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+        home: MainPage(),
+      );
+    });
+  }
+}
